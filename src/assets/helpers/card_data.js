0fef5c3c8d4da0710bc80d0855652ed1165be720
jauxsh <joshua.jih.chang@gmail.com>
2018-01-03 14:58:16 -0800

    initial setup complete

---
+++ src/assets/helpers/card_data.js
@@ -0,0 +1,26 @@
+import back from '../images/back.png';
+import react from '../images/react.svg';
+import shiba from '../images/shiba.png';
+
+export default [
+    {
+        back: back,
+        flipped: false,
+        front: react
+    },
+    {
+        back: back,
+        flipped: false,
+        front: react
+    },
+    {
+        back: back,
+        flipped: false,
+        front: shiba
+    },
+    {
+        back: back,
+        flipped: false,
+        front: shiba
+    }
+]
