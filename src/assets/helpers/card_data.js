2f452c29abc021b1542e315de97475b884f0fecd
jauxsh <joshua.jih.chang@gmail.com>
2018-01-03 19:40:37 -0800

    added more cards and functionality

--- src/assets/helpers/card_data.js
+++ src/assets/helpers/card_data.js
@@ -1,26 +1,18 @@
 import back from '../images/back.png';
-import react from '../images/react.svg';
-import shiba from '../images/shiba.png';
+import images from './card_images';
 
-export default [
-    {
-        back: back,
-        flipped: false,
-        front: react
-    },
-    {
-        back: back,
-        flipped: false,
-        front: react
-    },
-    {
-        back: back,
-        flipped: false,
-        front: shiba
-    },
-    {
-        back: back,
-        flipped: false,
-        front: shiba
-    }
-]
+export default images.map(front => ({
+    front,
+    back,
+    flipped: false
+})
+
+);
+
+// export default images.map( (value, index) => {
+//     return {
+//         back: back,
+//         front: value,
+//         flipped: false
+//     }
+// });
---
+++ src/assets/helpers/card_images.js
@@ -0,0 +1,16 @@
+import react from '../images/react.svg';
+import shiba from '../images/shiba.png';
+import git from '../images/github_logo.png';
+import css from '../images/css3.png';
+import js from '../images/js-icon.png';
+import node from '../images/nodejs.png';
+
+
+export default [
+    react,
+    shiba,
+    js,
+    css,
+    node,
+    git
+]
